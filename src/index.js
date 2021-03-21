541ef09c7f90a7d94388ed49b82f13abfea65aa4
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-03-21 22:54:23 +0900

    Fix: We shouldn’t be using hydrate yet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import App from "./App"
 import ReactDOM from "react-dom"
 
-ReactDOM.hydrate(<App />, document.getElementById("root"))
+// ReactDOM.hydrate(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
